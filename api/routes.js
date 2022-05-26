84158fed595b184c553eb65a931fcd239140ddb9
xpanvictor <xpanvictor@gmail.com>
2022-05-26 07:02:01 +0100

    Final routes

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,11 @@
-var routes = require('express').Router;
+var routes = require('express').Router();
 
 var controllers = require('./controllers');
 
+// The about request
 routes.get('/about', controllers.about);
+
+// The distance request
+routes.get('/distance', controllers.get_distance);
 
 module.exports = routes;
